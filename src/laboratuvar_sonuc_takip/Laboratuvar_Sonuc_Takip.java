580383261893c74edd41ffa77f1d489546aa1fc9
Mert AGCAKOYUN <mert.agcakoyun@stu.fsm.edu.tr>
2018-12-16 12:20:55 +0300

    First Commit

---
+++ src/laboratuvar_sonuc_takip/Laboratuvar_Sonuc_Takip.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratuvar_sonuc_takip;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Laboratuvar_Sonuc_Takip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       }
+    
+}
---
+++ test/BloodAnalysisTest.java
@@ -0,0 +1,18 @@
+
+import laboratuvar_sonuc_takip.BloodAnalysis;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BloodAnalysisTest {
+    public static void main(String[] args) {
+      
+    }
+}
